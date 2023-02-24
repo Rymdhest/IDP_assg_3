1063d498bb1c846b2c4e7490ca56ccfb88818549
Gustav Holmström <gustav.holmstroem@gmail.com>
2023-02-24 09:35:05 +0100

    lägger till syre, järn och guld.

--- IDP_assg_3/Program.cs
+++ IDP_assg_3/Program.cs
@@ -12,7 +12,9 @@
         }
         static void Main(string[] args)
         {
-            
+            Element oxygien = new Element() { name = "Syre", z = 8, type = "Ickemetall", meltingPoint = 54.36, boilingPoint = 90.188};
+            Element iron = new Element() { name = "Järn", z = 26, type = "Metall", meltingPoint = 1811, boilingPoint = 3134 };
+            Element gold = new Element() { name = "Guld", z = 79, type = "Metall", meltingPoint = 1337.33, boilingPoint = 3243 };
         }
     }
 }
