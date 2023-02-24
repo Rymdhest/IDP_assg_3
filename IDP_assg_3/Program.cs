1a7c8c65cb73d236e37c1dcce03bec1c207615aa
Gustav Holmström <gustav.holmstroem@gmail.com>
2023-02-24 09:16:46 +0100

    Add project files.

---
+++ IDP_assg_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IDP_assg_3
+{
+    internal class Program
+    {
+        class Element
+        {
+            public string name;
+            public int z;
+            public string type;
+            public double meltingPoint;
+            public double boilingPoint;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
